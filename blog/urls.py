aaeee754501ace8cdd092b4ffea1203a1c53b354
Manuel <manuelhany@gmail.com>
2023-10-23 14:56:17 +0300

    adding basic url functionality using normal method

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views 
-
-posts_urls = [
-    path("", views.all_blog_posts, name="all_blog_posts"),
-    path("<str:slug>", views.full_blog_post, name="full_blog_post"),
-]
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts/", include(posts_urls))
-    
+    path("posts", views.all_blog_posts, name="all_blog_posts"),
+    path("posts/<str:slug>", views.full_blog_post, name="full_blog_post"),
 ]
