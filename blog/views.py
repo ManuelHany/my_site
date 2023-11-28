673f493fa87c2f7d6d5a364a3350cac48ceb0341
Manuel <manuelhany@gmail.com>
2023-11-28 11:57:02 +0200

    servine comment form service

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.views import View
 
 from collections import OrderedDict
 from .models import Post
@@ -28,14 +29,30 @@ class PostsView(ListView):
     ordering = ["-date"]
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "blog/post-detail.html"
-    # context_object_name = "post" ==> no need here because the model is named Post itself.
+class PostDetailView(View):
+    def get(seld, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm
+        }
+        return render(request, "blog/post-detail.html", context)
+    
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
 
