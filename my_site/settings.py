6b85e538b6b91af4f7428d23146e31bc4b2b544f
Manuel <manuelhany@gmail.com>
2023-12-03 18:15:10 +0200

    Started Deployment, added the NotesToBeAdded, Collecting Static, Setting debug to False

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ppv7u)%ga6v*jj1)nhh^#8e-ezg+sza*cw1jkr%m%g&59^-7ev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
